389d3aa64c1428ce9406d49992aaa3ad53d264af
rinaz_a <rinaz_a@epitech.eu>
2015-11-13 11:47:38 +0100

    olol

---
+++ olol.c
@@ -0,0 +1,11 @@
+/*
+** olol.c for olol in /home/rinaz_a/TMTC/Selim
+** 
+** Made by selim rinaz
+** Login   <rinaz_a@epitech.net>
+** 
+** Started on  Fri Nov 13 11:45:33 2015 selim rinaz
+** Last update Fri Nov 13 11:45:43 2015 selim rinaz
+*/
+
+try it
